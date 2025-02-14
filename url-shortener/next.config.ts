f7b1515b8629eec0281191bcf13f9ceacb4af6e1
jesusparfer27 <jesusparfer@alumnos.cei.es>
2025-02-14 16:33:24 +0100

    commit

--- url-shortener/next.config.ts
+++ url-shortener/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,  // Habilitar el modo estricto de React
+  swcMinify: true,        // Habilitar el minificado con SWC
+  images: {
+    domains: ['example.com'],  // Si usas imágenes externas, define los dominios permitidos
+  },
 };
 
 export default nextConfig;
